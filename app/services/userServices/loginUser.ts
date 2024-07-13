0f839300421e5a7167fc2aa99469de56f9b1c41a
Aleksej Ignatenko <ignatenkoaleksej771@gmail.com>
2024-07-13 12:36:27 +0300

    Добавлено сохранение jwt-token в cookie.

--- app/services/userServices/loginUser.ts
+++ app/services/userServices/loginUser.ts
@@ -1,34 +1,35 @@
-export interface LoginUserModelRequests{
-    email: string;
-    password: string;
+import Cookies from 'js-cookie';
+
+export interface LoginUserModelRequests {
+  email: string;
+  password: string;
 }
 
 export const loginUser = async (userRequest: LoginUserModelRequests) => {
-    try {
-      const response = await fetch("https://localhost:7103/Users/login", {
-        method: "POST",
-        headers: {
-          "content-type": "application/json",
-        },
-        body: JSON.stringify(userRequest),
-      });
-  
-      if (response.ok) {
-        // If the response is successful (status 2xx), get the JWT token
-        const token = await response.json();
-        if (token) {
-            console.log(token);
-            localStorage.setItem('', token);
-            const jwtToken = localStorage.getItem('jwtToken');
-        }
-      } else {
-        // If the response is not successful, check the status and handle the error
-        const errorMessage = await response.text();
-        console.error('Error creating user:', errorMessage);
+  try {
+    const response = await fetch("https://localhost:7103/Users/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(userRequest),
+    });
+
+    if (response.ok) {
+      // If the response is successful (status 2xx), get the JWT token
+      const token = await response.json();
+      if (token) {
+        const expirationTime = new Date(new Date().getTime() + 24 * 60 * 60 * 1000); // Token is valid for 24 hours
+        Cookies.set('jwtToken', token, { expires: expirationTime });
       }
-    } catch (error) {
-      console.error('Error fetching:', error);
-      // Handle any errors that occurred during the request
-      throw error;
+    } else {
+      // If the response is not successful, check the status and handle the error
+      const errorMessage = await response.text();
+      console.error("Error creating user:", errorMessage);
     }
-  };
+  } catch (error) {
+    console.error("Error fetching:", error);
+    // Handle any errors that occurred during the request
+    throw error;
+  }
+};
