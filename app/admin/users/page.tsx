21ef88eb074cb87fd032808036a401298ceed007
Aleksej Ignatenko <ignatenkoaleksej771@gmail.com>
2024-07-19 12:52:30 +0300

    Изменен вывод пользователей.

--- app/admin/users/page.tsx
+++ app/admin/users/page.tsx
@@ -3,10 +3,11 @@
 import styles from './styles.module.css';
 import { useEffect, useState, useRef } from "react";
 import { fetchAllUsersAsync, User } from '../../services/userServices/fetchAllUsersAsync';
+import Link from 'next/link';
 
 export default function Users() {
   const hasBeenCalledRef = useRef(false);
-const [userData, setUserData] = useState<User[]>([]);
+  const [userData, setUserData] = useState<User[]>([]);
 
   useEffect(() => {
     if (!hasBeenCalledRef.current) {
@@ -26,24 +27,29 @@ const [userData, setUserData] = useState<User[]>([]);
     }
   }, []);
 
-  return (
+   return (
     <main className={styles.main}>
-        <div className={styles.backgroundContainer}>
-          <div className={styles.firstRow}>
-              {userData.map((user, index) => (
-              <div key={index} className={styles.card}>
-                  <div className={styles.container}>
-                  <div className={styles.content}>
-                      <h3>Id: {user.idUser}</h3>
-                      <h3>User name: {user.userName}</h3>
-                      <h3>Email: {user.email}</h3>
-                      <h3>Role: {user.role}</h3>
-                  </div>
-                  </div>
+      <div className={styles.backgroundContainer}>
+        <div className={styles.firstRow}>
+          {userData.map((user, index) => (
+            <div key={index} className={styles.card}>
+              <div className={styles.container}>
+                <div className={styles.content}>
+                  <h3>Id: {user.idUser}</h3>
+                  <h3>User name: {user.userName}</h3>
+                  <h3>Email: {user.email}</h3>
+                  <h3>Role: {user.role}</h3>
+                </div>
               </div>
-              ))}
-          </div>
+            </div>
+            ))}
         </div>
+      </div>
+      <div className={styles.backgroundContainer}>
+        <div className={styles.firstRow}>
+          
+        </div>
+      </div>
     </main>
   );
 }
